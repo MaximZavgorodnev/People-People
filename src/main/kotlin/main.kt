fd5d45ea9f94802273ebd37a24576ab69b5e5867
MaximZavgorodnev <maxpek9@yandex.ru>
2021-10-17 18:56:15 +0300

    Откорректировано когда количество 11,111,211 и тд.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,20 @@
 import java.util.*
 
 fun main(){
-    println("Введите количество человек которым понравилось")
-    val scan = Scanner(System.`in`)
-    val result: String
-    val likes = scan.nextInt() // Ввод количества лайков
-    if (likes % 10 === 1){
-        result = "человеку"
-        println("Понравилось $likes $result")
-    } else {
-        result = "людям"
-        println("Понравилось $likes $result")
+    while (true) {
+        println("Введите количество человек которым понравилось")
+        val scan = Scanner(System.`in`)
+        var result: String
+        val likes = scan.nextInt() // Ввод количества лайков
+        if (((likes/10) % 10) === 1) {
+            result = "людям"
+            println("Понравилось $likes $result")
+        } else if (likes % 10 === 1) {
+            result = "человеку"
+            println("Понравилось $likes $result")
+        } else {
+            result = "людям"
+            println("Понравилось $likes $result")
+        }
     }
 }
